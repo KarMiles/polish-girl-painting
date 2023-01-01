f5b62643c41b1e15b73f432c1cba81076e79693c
Karol Mileszko <karol.miles@gmail.com>
2023-01-01 10:34:18 +0000

    Fix search functionality

--- products/views.py
+++ products/views.py
@@ -12,17 +12,22 @@ def all_products(request):
     """
 
     products = Product.objects.all()
-    
+    query = None
+
     if request.GET:
         query = request.GET['q']
         if not query:
             messages.error(request, "Enter search criteria!")
             return redirect(reverse('products'))
-        
-        queries = Q(title__icontains=query) |
+
+        queries = Q(title__icontains=query) | \
+            Q(excerpt__icontains=query) | \
+            Q(description__icontains=query)
+        products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
