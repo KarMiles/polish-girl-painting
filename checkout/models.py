56f8ed6b76ad03a9808b863d1b23f08fd218e067
Karol Mileszko <karol.miles@gmail.com>
2023-01-11 22:14:18 +0000

    Add methods in checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
-from profiles.models import UserProfile
+# from profiles.models import UserProfile
 
 
 class Order (models.Model):
@@ -15,12 +15,12 @@ class Order (models.Model):
         max_length=32,
         null=False,
         editable=False)
-    user_profile = models.ForeignKey(
-        UserProfile,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='orders')
+    # user_profile = models.ForeignKey(
+    #     UserProfile,
+    #     on_delete=models.SET_NULL,
+    #     null=True,
+    #     blank=True,
+    #     related_name='orders')
     # Notes
     # Add related name of orders to access the users
     # by calling something like user.userprofile.orders
@@ -88,6 +88,37 @@ class Order (models.Model):
         blank=False,
         default='')
 
+    def _generate_order_number(self):
+        """
+        Generate a random unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+    
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery cost
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it has not been set already
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.order_number
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(
@@ -111,3 +142,14 @@ class OrderLineItem(models.Model):
         null=False,
         blank=False,
         editable=False)
+    
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it has not been set already
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
