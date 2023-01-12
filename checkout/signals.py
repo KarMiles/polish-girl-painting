2298835fbee5186bc593dfdba8f96c30c0764c8d
Karol Mileszko <karol.miles@gmail.com>
2023-01-12 20:01:05 +0000

    Add update_on_save and update_on_delete functions in checkout/signals.py

---
+++ checkout/signals.py
@@ -0,0 +1,32 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create.
+    Functioon which handles signals
+    from the post_save event.
+    Args:
+        sender: OrderLineItem,
+        instance: actual instance of the model that sent it,
+        created (bool): new item? (or one being updated?),
+        **kwargs
+    Returns:
+        Access instance.order (the order this specific
+        line item is related to),
+        Call update_total() method on it.
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete.
+    """
+    # print('delete signal received!')
+    instance.order.update_total()
