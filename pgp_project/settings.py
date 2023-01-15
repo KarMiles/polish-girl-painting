d3c49373d442e4f2720718cdefb171bc17b5b0f9
Karol Mileszko <karol.miles@gmail.com>
2023-01-15 13:29:33 +0000

    Add basic webhooks for testing

--- pgp_project/settings.py
+++ pgp_project/settings.py
@@ -192,7 +192,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Stripe
 STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
 
 # E-Commerce settings
