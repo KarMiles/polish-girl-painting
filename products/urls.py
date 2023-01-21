d8f438f7e37756933bd07128bff8815f9e19deb3
Karol Mileszko <karol.miles@gmail.com>
2023-01-21 18:28:02 +0000

    Add editing products functionality

--- products/urls.py
+++ products/urls.py
@@ -14,4 +14,8 @@ urlpatterns = [
         'add/',
         views.add_product,
         name='add_product'),
+    path(
+        'edit/<int:product_id>/',
+        views.edit_product,
+        name='edit_product'),
 ]
