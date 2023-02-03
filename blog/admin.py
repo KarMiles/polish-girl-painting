5773cb80282be37c422d4b4023cf2fad05268238
Karol Mileszko <karol.miles@gmail.com>
2023-02-03 21:30:35 +0000

    Create admin.py for blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,36 @@
+"""Imports"""
+# 3rd party:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from .models import Post
+
+
+# Models for blog management in admin page
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Admin class for the blog model.
+    Section for managing posts.
+    """
+    list_display = (
+        'title',
+        'live',
+        'highlight',
+        'created_on',
+        'updated_on')
+    search_fields = [
+        'title',
+        'content']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+    list_filter = (
+        'live',
+        'highlight')
+
+# Decoration replaces class:
+# admin.site.register(Post)
