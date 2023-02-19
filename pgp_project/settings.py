c6510b3ef6586dd5bb8a2aa7d203df1221640364
Karol Mileszko <karol.miles@gmail.com>
2023-02-19 19:35:22 +0000

    Unify database usage on Github and Heroku to postgres

--- pgp_project/settings.py
+++ pgp_project/settings.py
@@ -162,6 +162,10 @@ else:
         }
     }
 
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('BASE_DIR'))
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
