b6e537ae81f3ba47a33e6f8bbf1fc760cb9eb337
Karol Mileszko <karol.miles@gmail.com>
2023-03-01 15:54:58 +0000

    Minor styling changes to form borders

--- products/forms.py
+++ products/forms.py
@@ -4,6 +4,9 @@
 from django import forms
 from django_summernote.widgets import SummernoteWidget
 from .widgets import CustomClearableFileInput
+
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from .models import Product, Category
 
 
@@ -24,10 +27,15 @@ class ProductForm(forms.ModelForm):
         widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
+        """
+        Get friendly names for product categories
+        Apply classes to form fields
+        """
         super().__init__(*args, **kwargs)
+
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
         self.fields['category'].choices = friendly_names
+
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-custom corners'
