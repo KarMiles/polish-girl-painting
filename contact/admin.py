9a711c83148bff7d40441b0de3fc9955cfd5ec0d
Karol Mileszko <karol.miles@gmail.com>
2023-03-15 19:01:28 +0000

    Create admin.py for contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,21 @@
+"""Imports"""
+# 3rd party:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from django.contrib import admin
 
-# Register your models here.
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from .models import Contact
+
+# Admin model for contact app
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Admin class for the contact model.
+    """
+    list_display = ('name', 'email', 'subject', 'created_on')
+    search_fields = ['name', 'body']
+    list_filter = ('name', 'email', 'created_on')
+    list_display_links = ('subject',)
